5df3f682c5394e2851faabbac5705c5ecebcdec7
pentaled <105159143@student.swin.edu.au>
2025-05-20 16:46:11 +1000

    feat: Add json file to store product details

--- frontend/src/pages/StoreCatalog.tsx
+++ frontend/src/pages/StoreCatalog.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Product, StoreCatalogue } from '../models/Product';
+import products from '../data/products.json';
 
 const catalog = new StoreCatalogue();
-catalog.addProduct(new Product('1', 'Arduino UNO R4 Minima', 19.99, 'A versatile microcontroller board with built-in USB connectivity.'));
-catalog.addProduct(new Product('2', 'PIR (Motion) Sensor', 49.99, 'PIR motion sensor for detecting human presence.'));
-catalog.addProduct(new Product('3', 'Temperature Sensor', 49.99, 'Temperature sensor for measuring ambient temperature.'));
-catalog.addProduct(new Product('4', 'Analog Ambient Light Sensor', 49.99, 'Analog ambient light sensor for measuring light levels.', false));
+products.forEach((product: any) => {
+  catalog.addProduct(new Product(
+    product.id,
+    product.name,
+    product.price,
+    product.description,
+    product.available
+  ));
+});
 
 const catalogElement = catalog.renderCatalog();
 
 const StoreCatalogComponent: React.FC = () => {
-
   return (
     <div>{catalogElement}</div>
   );
